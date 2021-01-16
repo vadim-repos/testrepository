f95da11c92b30fce9c2093fbd8539979232a2c1c
Vadim <vivace88@seznam.cz>
2021-01-16 19:46:14 +0100

    initial commit

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ToDoApp.Models;
+using ToDoApp.Services;
 
 namespace ToDoApp
 {
@@ -20,9 +11,47 @@ namespace ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<ToDoModel> _toDoData;
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\toDoDataList.json";
+        private FileIOService _fileIOService;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _toDoData = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+            
+            dgToDoList.ItemsSource = _toDoData;
+            _toDoData.ListChanged += ToDoDataListChanged;
+        }
+
+        private void ToDoDataListChanged (object sender, ListChangedEventArgs e)
+        {
+            if(e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemChanged || e.ListChangedType == ListChangedType.ItemDeleted)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    Close();
+                }                
+            }
         }
     }
 }
---
+++ ToDoApp/Services/FileIOService.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.IO;
+using ToDoApp.Models;
+
+namespace ToDoApp.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+
+        public BindingList<ToDoModel> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+
+            if(!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<ToDoModel>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<ToDoModel>>(fileText);
+            }
+        }
+
+        public void SaveData(object list)
+        {
+            using (StreamWriter streamWriter = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(list);
+                streamWriter.Write(output);
+            }
+        }
+    }
+}
